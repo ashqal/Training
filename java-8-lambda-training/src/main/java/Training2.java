cdd2c9ff9d0fe7484c3b51af3dd36c1644303ad6
asha <ashnb@qq.com>
2015-12-14 14:31:31 +0800

    2.7 2 ThreadLocal

--- java-8-lambda-training/src/main/java/Training2.java
+++ java-8-lambda-training/src/main/java/Training2.java
@@ -1,11 +1,30 @@
+import java.util.function.Supplier;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
 public class Training2 {
     public interface Function<T,R> {
         R apply(T t);
     }
+
     public Training2(){
         Function<Long,Long> func = x -> x + 1;
-        Long result = func.apply(12L);
-        System.out.println("result:" + result);
+        System.out.println(String.format("result:%d",func.apply(12L)));
+        Supplier<DateFormat> supplier = () -> new SimpleDateFormat("dd-MM-YYYY");
+        int i = 0;
+        while ( i++ < 10 ) {
+            new Thread(() -> {
+                ThreadLocal<DateFormat> local = ThreadLocal.withInitial(supplier);
+                DateFormat formatter = local.get();
+                String date = formatter.format(new Date());
+                System.out.println(String.format("Thread:%s Date:%s",Thread.currentThread(),date));
+            }).start();
+        }
+        try{
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // do nothing.
+        }
     }
     public static void main(String[] args) {
         new Training2();
