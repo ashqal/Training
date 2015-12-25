f84543aab32cc2c626db6b61ef6369dc1d606929
asha <ashnb@qq.com>
2015-12-25 16:52:00 +0800

    chapter 5

---
+++ java-8-lambda-training/src/main/java/Training5.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.maxBy;
+
+/**
+ * Created by Asha on 15/12/25.
+ * Chapter 5
+ */
+public class Training5 {
+
+    public Training5() {
+        String str1 = "I love the world. He he";
+        String str2 = "I love the world.";
+        List<String> samples1 = asList("Waiting","For","You");
+
+        System.out.println("\n====== 5.6 1 a");
+        List<String> samples1Result = samples1.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(samples1Result);
+
+        System.out.println("\n====== 5.6 1 b");
+        int count = samples1.stream().map(String::length).reduce(0, Integer::sum);
+        System.out.println(count);
+
+        System.out.println("\n====== 5.6 1 c");
+        List<Integer> resultOfFlatMap = Stream.of(asList(1,2),asList(3,4)).flatMap(List::stream).collect(Collectors.toList());
+        System.out.println(resultOfFlatMap);
+
+        System.out.println("\n====== 5.6 2 a");
+        List<String> nameList = asList("John Lennon","Paul McCarteny","George Harrison","Ringo Starr","Pete Best","Stuart Sutcliffe");
+        Optional<String> longestName = nameList.stream().reduce((acc, data) -> acc.length() > data.length() ? acc : data);
+        System.out.println(longestName.orElse("unknown"));
+
+        longestName = nameList.stream().collect(maxBy(Comparator.comparing(String::length)));
+        System.out.println(longestName.orElse("unknown"));
+
+
+    }
+
+    public static void main(String[] args) {
+        new Training5();
+    }
+}
